881341f07c260183c341a5822495d0fb6d8c53ff
nanometre <benedict.wong@trentglobal.edu.sg>
2022-02-19 14:14:18 +0000

    Added minor changes to scripts

--- scripts/data.js
+++ scripts/data.js
@@ -1,38 +1,12 @@
-// using a single loadGeoJSON function
-
-// async function loadGeoJSON(geoJSON){
-//     let response = await axios.get(geoJSON);
-//     let group = L.markerClusterGroup();
-//     L.geoJson(response.data, {
-//         'onEachFeature': function(feature, marker){
-//             marker.bindPopup(feature.properties.Description);
-//         }
-//     }).addTo(group);
-//     group.addTo(map);
-//     return group
-// }
-
-// separating the loading of geoJson data
-
-const historicSiteIcon = L.icon({
-    iconUrl: './images/icons/site.png',
-    iconSize: [50, 50],
-})
-
-const monumentIcon = L.icon({
-    iconUrl: './images/icons/monument.png',
-    iconSize: [50, 50],
-})
-
-const museumIcon = L.icon({
-    iconUrl: './images/icons/museum.png',
-    iconSize: [50, 50],
-})
+async function getData(dataURL){
+    let response = await axios.get(dataURL);
+    return response.data
+}
 
 async function loadHistoricSite() {
-    let response = await axios.get('data/historic-sites-geojson.geojson');
+    let historicSiteData = await getData('data/historic-sites-geojson.geojson');
     let historicSiteGroup = L.markerClusterGroup()
-    L.geoJson(response.data, {
+    L.geoJson(historicSiteData, {
         'onEachFeature': function (feature, marker) {
             marker.bindPopup(feature.properties.Description)
         },
@@ -45,9 +19,9 @@ return historicSiteGroup;
 }
 
 async function loadMonument() {
-    let response = await axios.get('data/monuments-geojson.geojson');
+    let monumentData = await getData('data/monuments-geojson.geojson');
     let monumentGroup = L.markerClusterGroup()
-    L.geoJson(response.data, {
+    L.geoJson(monumentData, {
         'onEachFeature': function (feature, marker) {
             marker.bindPopup(feature.properties.Description);
         },
@@ -60,9 +34,9 @@ async function loadMonument() {
 }
 
 async function loadMuseum() {
-    let response = await axios.get('data/museums-geojson.geojson');
+    let museumData = await getData('data/museums-geojson.geojson')
     let museumGroup = L.markerClusterGroup()
-    L.geoJson(response.data, {
+    L.geoJson(museumData, {
         'onEachFeature': function (feature, marker) {
             marker.bindPopup(feature.properties.Description);
         },
@@ -75,10 +49,11 @@ async function loadMuseum() {
 }
 
 async function loadWeather2H() {
-    let response = await axios.get('https://api.data.gov.sg/v1/environment/2-hour-weather-forecast');
+    // let response = await axios.get('https://api.data.gov.sg/v1/environment/2-hour-weather-forecast');
+    let weather2HData = await getData('https://api.data.gov.sg/v1/environment/2-hour-weather-forecast')
     let weather2HGroup = L.markerClusterGroup();
-    let weatherCoordinates = response.data.area_metadata;
-    let weatherForecast = response.data.items[0].forecasts;
+    let weatherCoordinates = weather2HData.area_metadata;
+    let weatherForecast = weather2HData.items[0].forecasts;
     for (let i = 0; i < weatherCoordinates.length; i++) {
         let lat = weatherCoordinates[i].label_location.latitude;
         let lng = weatherCoordinates[i].label_location.longitude;
--- scripts/map.js
+++ scripts/map.js
@@ -9,3 +9,18 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
 }).addTo(map);
+
+const historicSiteIcon = L.icon({
+    iconUrl: './images/icons/site.png',
+    iconSize: [45, 45],
+})
+
+const monumentIcon = L.icon({
+    iconUrl: './images/icons/monument.png',
+    iconSize: [45, 45],
+})
+
+const museumIcon = L.icon({
+    iconUrl: './images/icons/museum.png',
+    iconSize: [45, 45],
+})
